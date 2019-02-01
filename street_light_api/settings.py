b3af115cf857694e09768c30307a8fac8580de75
Akshansh Jain <jnakshansh@gmail.com>
2019-02-01 16:18:56 +0530

    final final

--- street_light_api/settings.py
+++ street_light_api/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 import socket
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,4 +131,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ###
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
