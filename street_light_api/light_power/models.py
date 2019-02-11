195dccf6469ad8044bc63e16a74d4fbab2ccd87e
Akshansh Jain <jnakshansh@gmail.com>
2019-02-11 17:55:31 +0530

    Timezone

--- street_light_api/light_power/models.py
+++ street_light_api/light_power/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from datetime import datetime
+from pytz import timezone
 # Create your models here.
 class power(models.Model):
     # id = models.IntegerField(primary_key=True)
     power_consumption = models.IntegerField()
-    date_time_record = models.DateTimeField(default=datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    date_time_record = models.DateTimeField(default=datetime.now(timezone('Asia/Kolkata')).strftime('%Y-%m-%d %H:%M:%S'))
     def __str__(self):
         return str(self.power_consumption)
